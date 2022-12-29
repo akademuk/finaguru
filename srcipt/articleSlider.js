18f9a858309a8b9a020f31c3365cf2a1bcfa7852
akademuk <akademuk24@gmail.com>
2022-12-29 15:45:29 +0200

    ss

---
+++ srcipt/articleSlider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.articleSlider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        prevArrow: $('.articleSliderPrev'),
+        nextArrow: $('.articleSliderNext'),
+    });
+});
