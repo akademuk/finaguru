c79c3a27986e6a3b15366cdac97d00c8ff3a140d
akademuk <akademuk24@gmail.com>
2022-12-29 11:30:40 +0200

    sss

--- srcipt/sliderPartners.js
+++ srcipt/sliderPartners.js
@@ -1,28 +1,29 @@
-// $(document).ready(function(){
-//     $('.partnersSlider').slick({
-//         infinite: true,
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         dots: false,
-//         arrows: false,
-//         prevArrow: $('.partnersSliderPrev'),
-//         nextArrow: $('.partnersSliderNext'),
-//         appendDots: $('.partnersSliderDots'),
-//         mobileFirst: true,
-//         responsive: [
-//             {
-//                 breakpoint: 9999,
-//                 settings: "unslick"
-//             },
-//             {
-//                 breakpoint: 767,
-//                  settings: {
-//                         slidesToShow: 2,
-//                         slidesToScroll: 1,
-//                         infinite: true,
-//                         dots: true
-//                     }
-//             }
-//         ]
-//     });
-// });
+$(document).ready(function(){
+    $('.partnersSlider').slick({
+        // infinite: true,
+        // slidesToShow: 3,
+        // slidesToScroll: 1,
+        // dots: false,
+        // arrows: false,
+        // prevArrow: $('.partnersSliderPrev'),
+        // nextArrow: $('.partnersSliderNext'),
+        // appendDots: $('.partnersSliderDots'),
+        settings: "unslick",
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 9999,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 767,
+                 settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+            }
+        ]
+    });
+});
